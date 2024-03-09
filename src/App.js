8fa8e4a003bf31d55a0de0b6d4efca21010a9c47
9912353870 <sravan8147@gmail.com>
2024-03-09 22:37:35 +0530

    Added changes socket.io file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { socket } from "./socket";
+import { socket } from "./services/socket";
 
 export default function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  // const [isConnected, setIsConnected] = useState(socket.connected);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+  // useEffect(() => {
+  //   function onConnect() {
+  //     setIsConnected(true);
+  //   }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+  //   function onDisconnect() {
+  //     setIsConnected(false);
+  //   }
 
-    socket.connect();
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
+  //   socket.connect();
+  //   socket.on("connect", onConnect);
+  //   socket.on("disconnect", onDisconnect);
 
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.disconnect();
-    };
-  }, []);
+  //   return () => {
+  //     socket.off("connect", onConnect);
+  //     socket.off("disconnect", onDisconnect);
+  //     socket.disconnect();
+  //   };
+  // }, []);
 
   return <div>{`Hello user: ${socket.id}`}</div>;
 }
