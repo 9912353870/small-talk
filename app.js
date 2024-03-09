f35421024c279b58e3863fdbcddc5173b79550b3
9912353870 <sravan8147@gmail.com>
2024-03-09 16:58:12 +0530

    Adding and removing peer connections

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
+let connectedPeers = [];
+
 app.use(express.static("public"));
 
 io.on("connect", (socket) => {
-  console.log("Socket server is running..!", { id: socket.id });
+  connectedPeers.push(socket.id);
+  console.log("Peers connected: ", connectedPeers);
+  
+  socket.on("disconnect", () => {
+    console.log("Peers disconnected: ", connectedPeers);
+    const newConnectedPeers= connectedPeers.filter(peerId => peerId !== socket.id);
+    connectedPeers = newConnectedPeers;
+  });
+
 });
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
